373181c7f513e75efa5029b87a1bccf5e0c715ee
Roberta <63109114+rmftelier@users.noreply.github.com>
2023-05-26 12:38:43 -0300

    POST e DELETE estão funcionando!

---
+++ Models/Carteira.cs
@@ -0,0 +1,26 @@
+﻿namespace atividade2.Models
+{
+    public class Carteira
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public float Saldo { get; set; }
+
+        public int MoedaId { get; set; }
+
+        //public Moeda Moeda { get; set; }
+
+
+        //Construtores
+        public Carteira() { }
+
+        public Carteira(string nome, float saldo, int moedaId)
+        {
+            Nome = nome;
+            Saldo = saldo;
+            MoedaId = moedaId;
+        }
+    }
+}
